ae7f38179bc5a63db2470aa26edb01f3afc90e46
Ben Munat <benm@kongregate.com>
2019-05-15 13:20:52 -0700

    Take optional open, close, error callbacks

--- src/components/SocketProvider.js
+++ src/components/SocketProvider.js
@@ -1,23 +1,36 @@
 import React, { useEffect } from 'react'
+
 import PropTypes from 'prop-types'
 import { Socket } from 'phoenix'
-
 import SocketContext from '../contexts/SocketContext'
 
-const SocketProvider = ({wsUrl, options, children}) => {
+const SocketProvider = ({ wsUrl, options, children }) => {
   const socket = new Socket(wsUrl, { params: options })
+  _attachCallbacks(socket, options)
 
   useEffect(() => { socket.connect() }, [options, wsUrl])
 
   return (
     <SocketContext.Provider value={socket}>
-      { children }
+      {children}
     </SocketContext.Provider>
-   )
- }
+  )
+}
+
+const _attachCallbacks = (socket, options) => {
+  if (options.onOpen) {
+    socket.onOpen((event) => { options.onOpen(event) })
+  }
+  if (options.onClose) {
+    socket.onError((event) => { options.onError(event) })
+  }
+  if (options.onError) {
+    socket.onError((event) => { options.onError(event) })
+  }
+}
 
 SocketProvider.defaultProps = {
- options: {}
+  options: {}
 }
 
 SocketProvider.propTypes = {
